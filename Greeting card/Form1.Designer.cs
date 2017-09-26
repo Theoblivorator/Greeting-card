0faa9398f5934a48731d4f3bb25187c0b5702a7c
temp <temp>
2017-09-26 13:06:33 -0400

    Add project files.

---
+++ Greeting card/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Greeting_card
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cardButton = new System.Windows.Forms.Button();
+            this.sureButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cardButton
+            // 
+            this.cardButton.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.cardButton.Font = new System.Drawing.Font("Calibri", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cardButton.ForeColor = System.Drawing.Color.Cyan;
+            this.cardButton.Location = new System.Drawing.Point(117, 121);
+            this.cardButton.Name = "cardButton";
+            this.cardButton.Size = new System.Drawing.Size(88, 44);
+            this.cardButton.TabIndex = 0;
+            this.cardButton.Text = "Go";
+            this.cardButton.UseVisualStyleBackColor = false;
+            this.cardButton.Click += new System.EventHandler(this.cardButton_Click);
+            // 
+            // sureButton
+            // 
+            this.sureButton.Font = new System.Drawing.Font("Papyrus", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sureButton.Location = new System.Drawing.Point(100, 62);
+            this.sureButton.Name = "sureButton";
+            this.sureButton.Size = new System.Drawing.Size(128, 149);
+            this.sureButton.TabIndex = 1;
+            this.sureButton.Text = "Are You Sure?";
+            this.sureButton.UseVisualStyleBackColor = true;
+            this.sureButton.Click += new System.EventHandler(this.Sure_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlText;
+            this.ClientSize = new System.Drawing.Size(321, 301);
+            this.Controls.Add(this.sureButton);
+            this.Controls.Add(this.cardButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cardButton;
+        private System.Windows.Forms.Button sureButton;
+    }
+}
+
---
+++ Greeting card/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace Greeting_card
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sureButton.Visible = false;
+        }
+            
+        private void cardButton_Click(object sender, EventArgs e)
+        {
+
+            Graphics formGraphics = this.CreateGraphics();
+            Pen drawPen = new Pen(Color.Brown, 10);
+            SolidBrush drawBrush = new SolidBrush(Color.Brown);
+
+            cardButton.Visible = false;
+            BackColor = Color.Green;
+            
+             sureButton.Visible = true;
+            
+            
+            
+        }
+
+        private void Sure_Click(object sender, EventArgs e)
+        {
+            sureButton.Visible = false;
+            Refresh();
+            Thread.Sleep(500);
+
+            Graphics formGraphics = this.CreateGraphics();
+            Pen drawPen = new Pen(Color.Blue, 10);
+            SolidBrush drawBrush = new SolidBrush(Color.Blue);
+
+            formGraphics.DrawLine(drawPen, 100, 225, 50, 75);
+        }
+    }
+}
